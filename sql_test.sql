ae4385336ce44e8fac64f4614ca93964ddbdd067
Francisco Rodrigues <francisco.rodrigue.rs@gmail.com>
2025-10-06 21:00:43 -0300

    add sql_test.sql to project and restore original db.sqlite

--- sql_test.sql
+++ sql_test.sql
@@ -1,70 +1,51 @@
-Teste Técnico de Conhecimentos em SQL
+-- desafio 1
+select ID_VENDEDOR as id,
+       NOME        as nome,
+       SALARIO     as salario
+from VENDEDORES
+where INATIVO = 0
+order by NOME ASC;
 
 
-Bem-vindo ao teste técnico de conhecimentos em SQL! A seguir, você encontrará cinco desafios onde precisará escrever consultas SQL. Utilize apenas as tabelas e dados presentes no arquivo de SQLs fornecido.
-Instruções
+-- desafio 2
+select ID_VENDEDOR as id,
+       NOME        as nome,
+       SALARIO     as salario
+from VENDEDORES
+where SALARIO >
+      (select AVG(SALARIO)
+       from VENDEDORES)
+order by SALARIO DESC;
 
+--desafio 3
+select C.ID_CLIENTE                  as id,
+       C.RAZAO_SOCIAL                as razao_social,
+       ifnull(SUM(P.VALOR_TOTAL), 0) as total
+from CLIENTES C
+         left join PEDIDO P on C.ID_CLIENTE = P.ID_CLIENTE
+group by C.ID_CLIENTE
+order by sum(P.VALOR_TOTAL) desc;
 
-Desafios: Resolva cada um dos desafios propostos escrevendo as queries SQL apropriadas.
-Retorno Esperado: Certifique-se de que as consultas retornem os resultados esperados, incluindo os nomes das colunas especificados.
-Validação: Após finalizar todas as consultas, valide suas respostas no compilador online de PostgreSQL: SQL Fiddle.
-Entrega: Submeta suas consultas finais em um repositório próprio seu!
-Desafios
+--desafio 4
+select p.ID_PEDIDO    as id,
+       p.VALOR_TOTAL  as valor,
+       p.DATA_EMISSAO as data,
+       CASE
+           WHEN DATA_CANCELAMENTO THEN 'CANCELADO'
+           WHEN DATA_FATURAMENTO THEN 'FATURADO'
+           WHEN DATA_FATURAMENTO IS NULL AND DATA_CANCELAMENTO IS NULL THEN 'PENDENTE'
+           END        as situacao
+from PEDIDO p;
 
-
-Consulta de Vendedores: Escreva uma query para listar todos os vendedores ativos, mostrando as colunas id, nome, salario. Ordene o resultado pelo nome em ordem ascendente.
-Retorno esperado: Selecionar a coluna Nome e definir a ordem para ascendente.
-id,nome,salario
-2,Vendedor B,4000.00
-4,Vendedor D,3800.00
-1,Vendedor Z,3000.00
-
-Funcionários com Salário Acima da Média: Escreva uma query para listar os funcionários que possuem um salário acima da média salarial de todos os funcionários. A consulta deve mostrar as colunas id, nome, e salário, ordenadas pelo salário em ordem descendente.
-Retorno esperado: Selecionar a coluna salário e definir a ordem para decrescente.
-
-
-id,nome,salario
-5,Vendedor E,4200.00
-2,Vendedor B,4000.00
-4,Vendedor D,3800.00
-
-Resumo por cliente: Escreva uma query para listar todos os clientes e o valor total de pedidos já transmitidos. A consulta deve retornar as colunas id, razao_social, total, ordenadas pelo total em ordem descendente.
-Retorno esperado: Selecionar a coluna total e definir a ordem para decrescente.
-id,razao_social,total
-4,Cliente D,530
-3,Cliente C,430
-2,Cliente B,350
-1,Cliente A,250
-5,Cliente E,0
-
-Situação por pedido: Escreva uma query que retorne a situação atual de cada pedido da base. A consulta deve retornar as colunas id, valor, data e situacao. A situacao deve obedecer a seguinte regra:
-Se possui data de cancelamento preenchido: CANCELADO
-Se possui data de faturamento preenchido: FATURADO
-Caso não possua data de cancelamento e nem faturamento: PENDENTE
-Retorno esperado: SELECIONE * DA TABELA ID IN (
-SELECIONE SITUAÇÃO ONDE (SITUAÇÃO = CANCELADO,FATURADO E PENDENTE)
-
-
-id,valor,data,situacao
-1,120.00,2023-07-06,PENDENTE
-2,130.00,2023-07-07,PENDENTE
-3,170.00,2023-07-08,FATURADO
-4,180.00,2023-07-09,CANCELADO
-5,210.00,2023-07-10,PENDENTE
-6,220.00,2023-07-11,FATURADO
-7,260.00,2023-07-12,CANCELADO
-8,270.00,2023-07-13,FATURADO
-
-Produtos mais vendidos: Escreva uma query que retorne o produto mais vendido ( em quantidade ), incluindo o valor total vendido deste produto, quantidade de pedidos em que ele apareceu e para quantos clientes diferentes ele foi vendido. A consulta deve retornar as colunas id_produto, quantidade_vendida, total_vendido, clientes, pedidos. Caso haja empate em quantidade de vendas, utilizar o total vendido como critério de desempate.
-Retorno esperado:
-id_produto,quantidade_vendida,total_vendido,pedidos,clientes
-2,12,220,3,2
-
-Requisitos
-
-
-Utilize apenas as tabelas e dados presentes no arquivo schema.sql.
-Certifique-se de que os nomes das colunas no resultado sejam exatamente os especificados e retornados nos desafios.
-Valide suas respostas utilizando o compilador online SQL Fiddle.
-Boa sorte!
-
+--desafio 5
+SELECT ip.id_produto                           as id_produto,
+       SUM(ip.QUANTIDADE)                      AS quantidade_vendida,
+       SUM(ip.QUANTIDADE * ip.PRECO_PRATICADO) AS total_vendido,
+       COUNT(DISTINCT ip.id_pedido)            AS pedidos,
+       COUNT(DISTINCT p.id_cliente)            AS clientes
+FROM itens_pedido ip
+         JOIN PEDIDO p ON p.id_pedido = ip.id_pedido
+GROUP BY ip.id_produto
+ORDER BY quantidade_vendida DESC,
+         total_vendido DESC
+LIMIT 1;
